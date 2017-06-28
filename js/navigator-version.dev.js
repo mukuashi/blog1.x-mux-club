19ffd3f3c197aadb4e579d284d3172d64e8bf84b
PhotoArtLife <572512250@qq.com>
2017-06-29 02:12:45 +0800

    update 2016-now sap web

--- js/navigator-version.dev.js
+++ js/navigator-version.dev.js
@@ -25,35 +25,35 @@ if(!IsPC()) {
 //首页版本切换-弹框提示
 swal({
   html: true,
-  title: '新版本上线了，来观摩一下么?<p>当前版本2017及之后 / 老版本2016及之前</p><br/><p>5秒后系统会默认为你选择老版本</p>',
+  title: '新版本上线了，来观摩一下么?<p>当前版本2017及之后 / 老版本2016及之前</p><br/><p>3秒后系统会默认为你选择老版本</p>',
   type: 'info',
   showCancelButton: true,
   confirmButtonColor: '#3085d6',
   cancelButtonColor: '#d33',
   cancelButtonText: '先老的吧',
   confirmButtonText: '去啊Go',
-  closeOnConfirm: false, 
+  closeOnConfirm: false,
   closeOnCancel: false,
-  timer: 5000
-},function(isConfirm){ 
+  timer: 3000
+},function(isConfirm){
   if (isConfirm) {
     swal({
       html: true,
       type: 'success',
       title: '1秒后自动跳转中...',
-      text: '', 
-      showConfirmButton: false        
+      text: '',
+      showConfirmButton: false
     },setTimeout(function() {
         window.location.href = 'http://kquanr.com/2016-now'
-    },1000));  
+    },1000));
   } else {
     swal({
       html: true,
       type: 'warning',
       title: '您主动或系统默认选择了老版本:)',
-      text: '3秒后自动关闭',      
-      confirmButtonText: '好的，OK', 
-      timer: 3000
+      text: '3秒后自动关闭',
+      confirmButtonText: '好的，OK',
+      timer: 2000
     });
   } 
 });
@@ -61,10 +61,10 @@ swal({
 document.getElementById('version').addEventListener("click", function(){
     swal({
       html: true,
-      timer: 5000,
-      title: 'Hey，欢迎访问 PhotoArtLife<a href="javascript;" style="display: block;text-decoration: none;color: #09bfac;font-weight: 400;">version: 1.x</a>', 
-      text: '这是2016年及之前的老版本，<a href="http://kquanr.com/2016-now" style="border:none;color: rgba(222, 67, 23, 0.76);font-weight: 400;">新版本 </a><br />可在此或首页选择访问（5秒后自动关闭）',
+      timer: 3500,
+      title: 'Hey，欢迎访问 PhotoArtLife<a href="javascript:;" style="display: block;text-decoration: none;color: #09bfac;font-weight: 400;">version: 1.x</a>',
+      text: '这是2016年及之前的老版本，<a href="http://kquanr.com/2016-now" style="border:none;color: rgba(222, 67, 23, 0.76);font-weight: 400;">新版本 </a><br />可在此或首页选择访问（3秒后自动关闭）',
       imageUrl: 'http://kquanr.com/images/now1.png',
-      confirmButtonText: '好的，OK' 
+      confirmButtonText: '好的，OK'
     });
 });
