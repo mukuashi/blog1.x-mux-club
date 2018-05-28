adac0a7690b05e0127b1d4394eddf765d5b84096
mukuashi <572512250@qq.com>
2018-05-29 04:22:57 +0800

    adjust scree

--- project/js/target.js
+++ project/js/target.js
@@ -1,199 +1,40 @@
 $(document).ready(function () {
     //新链接嵌套格式
     //弹出即全屏
+    $('.web_lofter').click(function () {
+        location.href = "http://www.lofter.com/app/photoartlife";
+    });
     $('.web_mall').click(function () {
         location.href = "http://kquanr.com/mall";
     });
     $('.web_jiacheng2').click(function () {
-        location.href = "http://kquanr.com/jiacheng2";
+        location.href = "http://kquanr.com/project/demo/jiacheng2";
     });
-    $('.web_lofter').click(function () {
-        location.href = "http://www.lofter.com/app/photoartlife";
-    });
-
     $('.web_jiacheng1').click(function () {
-        location.href = "http://kquanr.com/jiacheng1";
+        location.href = "http://kquanr.com/project/demo/jiacheng1";
     });
-
     $('.web_edu').click(function () {
         location.href = "http://kquanr.com/kaoshi";
     });
-
     $('.web_feidajie').click(function () {
-        location.href = "http://kquanr.com/feidajie";
+        location.href = "http://kquanr.com/project/demo/feidajie";
     });
-
-    $('.web_ttfeel').click(function () {
-        location.href = "http://ttfeel.cn";
-    });
-
-    $('.web_iydmb').click(function () {
-        location.href = "http://www.iydmb.com";
-    });
-
     $('.web_hiyo').click(function () {
-        location.href = "http://kquanr.com/hiyo";
+        location.href = "http://kquanr.com/project/demo/hiyo";
     });
-    $('.web_pcjc').click(function () {
-        location.href = "http://kquanr.com/jiacheng2";
-    });
-    $('.web_now').click(function () {
-        location.href = "http://kquanr.com/2.x";
-    });
-
-    $('.web_hushenghao').click(function () {
-        layer.open({
-            type: 2,
-            title: '胡圣浩博士移动网站',
-            shadeClose: true,
-            shade: 0.8,
-            area: ['380px', '90%'],
-            content: 'http://kquanr.com/hushenghao' //iframe的url
-        });
-    });
-
-    $('.web_jingnan').click(function () {
-        layer.open({
-            type: 2,
-            title: '景楠老师微官网',
-            shadeClose: true,
-            shade: 0.8,
-            area: ['380px', '90%'],
-            content: 'http://kquanr.com/jingnan' //iframe的url
-        });
-    });
-
-    $('.web_yangkeming').click(function () {
-        layer.open({
-            type: 2,
-            title: '杨克明博士微官网',
-            shadeClose: true,
-            shade: 0.8,
-            area: ['380px', '90%'],
-            content: 'http://kquanr.com/yangkeming' //iframe的url
-        });
-    });
-
-    $('.web_xuchen').click(function () {
-        layer.open({
-            type: 2,
-            title: '许辰老师手机网站',
-            shadeClose: true,
-            shade: 0.8,
-            area: ['380px', '90%'],
-            content: 'http://kquanr.com/xuchen' //iframe的url
-        });
-    });
-
-    $('.web_xujinyuan').click(function () {
-        layer.open({
-            type: 2,
-            title: '徐锦元老师移动网站',
-            shadeClose: true,
-            shade: 0.8,
-            area: ['380px', '90%'],
-            content: 'http://kquanr.com/xujinyuan' //iframe的url
-        });
-    });
-
-    $('.web_yangyonghui').click(function () {
-        layer.open({
-            type: 2,
-            title: '杨永会老师微官网',
-            shadeClose: true,
-            shade: 0.8,
-            area: ['380px', '90%'],
-            content: 'http://kquanr.com/yangyonghui' //iframe的url
-        });
-    });
-
-    $('.web_guwenhao').click(function () {
-        layer.open({
-            type: 2,
-            title: '顾文豪老师手机网站',
-            shadeClose: true,
-            shade: 0.8,
-            area: ['380px', '90%'],
-            content: 'http://kquanr.com/guwenhao' //iframe的url
-        });
-    });
-
-    $('.web_wangamang').click(function () {
-        layer.open({
-            type: 2,
-            title: '王阿芒讲师微官网',
-            shadeClose: true,
-            shade: 0.8,
-            area: ['380px', '90%'],
-            content: 'http://kquanr.com/wangamang' //iframe的url
-        });
-    });
-
-    $('.web_wangweiling').click(function () {
-        layer.open({
-            type: 2,
-            title: '王维玲老师微官网',
-            shadeClose: true,
-            shade: 0.8,
-            area: ['380px', '90%'],
-            content: 'http://kquanr.com/wangweiling' //iframe的url
-        });
-    });
-
-    $('.web_cuijin').click(function () {
-        layer.open({
-            type: 2,
-            title: '崔进老师微官网',
-            shadeClose: true,
-            shade: 0.8,
-            area: ['380px', '90%'],
-            content: 'http://kquanr.com/cuijin' //iframe的url
-        });
-    });
-
-    $('.web_liujin').click(function () {
-        layer.open({
-            type: 2,
-            title: '刘进老师微官网',
-            shadeClose: true,
-            shade: 0.8,
-            area: ['380px', '90%'],
-            content: 'http://kquanr.com/liujin' //iframe的url
-        });
-    });
-
     $('.web_baihua').click(function () {
-        layer.open({
-            type: 2,
-            title: '菏泽百花生物科技有限公司',
-            shadeClose: true,
-            shade: 0.8,
-            area: ['380px', '90%'],
-            content: 'http://kquanr.com/baihua' //iframe的url
-        });
+        location.href = "http://kquanr.com/project/demo/baihua";
     });
-
-    $('.web_liurongmei').click(function () {
-        layer.open({
-            type: 2,
-            title: '刘荣梅老师微官网',
-            shadeClose: true,
-            shade: 0.8,
-            area: ['380px', '90%'],
-            content: 'http://kquanr.com/liurongmei' //iframe的url
-        });
-    });
-
-    $('.web_zhangjiuyuan').click(function () {
-        layer.open({
-            type: 2,
-            title: '张九元老师移动网站',
-            shadeClose: true,
-            shade: 0.8,
-            area: ['380px', '90%'],
-            content: 'http://kquanr.com/zhangjiuyuan' //iframe的url
-        });
-    });
+    // $('.web_jingnan').click(function () {
+    //     layer.open({
+    //         type: 2,
+    //         title: '景楠老师微官网',
+    //         shadeClose: true,
+    //         shade: 0.8,
+    //         area: ['380px', '90%'],
+    //         content: 'http://kquanr.com/jingnan' //iframe的url
+    //     });
+    // });
 
     //不同颜色标签提示弹窗
     $('.color1').click(function (event) {
